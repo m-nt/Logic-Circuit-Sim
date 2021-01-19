073007b326c78c956d3556d07147effc56878763
mohamadnosratian <68176509+mohamadnosratian@users.noreply.github.com>
2021-01-19 11:00:02 -0600

    add creating button function and drag and drop function

--- lcs/src/App.js
+++ lcs/src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import NodeButt from './Component/nodetest'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      components: []
+    }
+  }
+  addcomponents() {
+    const newComponents = [...this.state.components, "NodeButt"];
+
+    this.setState({
+      components: newComponents
+    });
+  }
 
   render() {
     return (
       <div >
-        <NodeButt />
+        <button type="button" onClick={this.addcomponents.bind(this)} class="btn btn-outline-primary" style={{ position: "fixed", left: 50, top: 50 }}>Primary</button>
+        {this.state.components.map((item, i) => { console.log(item); return <NodeButt key={i} /> })}
       </div>
     );
   }
--- lcs/src/Component/nodetest.js
+++ lcs/src/Component/nodetest.js
@@ -3,15 +3,20 @@ import React, { Component } from 'react';
 class NodeButt extends Component {
     constructor(props) {
         super(props)
-        this.state = { x: 0, y: 0 }
-        this.ActiveState = false
+        this.state = { x: 0, y: 0, Active: true }
     }
     _onMouseMove(e) {
-        if (this.ActiveState == true) {
+        if (this.state.Active === true) {
             this.setState({ x: e.screenX - 20, y: e.screenY - 157 });
         }
     }
-
+    _mouse_click() {
+        if (this.state.Active === false) {
+            this.setState({ Active: true })
+        } else {
+            this.setState({ Active: false })
+        }
+    }
     render() {
         const { x, y } = this.state;
         return (
@@ -20,7 +25,7 @@ class NodeButt extends Component {
                 height: "2000px",
                 width: "2000px"
             }} onMouseMove={this._onMouseMove.bind(this)}>
-                <button draggable="true" onDrag={this.ActiveState = true}
+                <button draggable="true" onClick={this._mouse_click.bind(this)}
                     style={{ position: "fixed", left: x, top: y }}
 
                     type="button" class="btn btn-outline-primary">Primary</button>
