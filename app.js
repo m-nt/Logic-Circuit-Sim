6ca76e531fbee844d2af50e77bd8de62b6548607
mohamadnosratian <68176509+mohamadnosratian@users.noreply.github.com>
2021-01-20 18:54:23 -0600

    set up Canvas for Application

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const session = require("express-session");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("./Public"));
+//EJS engine setup
+app.use(expressLayout);
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+
+//Body Parser
+app.use(express.urlencoded({ extended: false }));
+
+//Express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//Routes set up
+app.use("/", require("./routes/index.js"));
 
 app.listen(PORT, () => {
   console.log(`app listening on port: ${PORT}`);
---
+++ public/app/main.js
@@ -0,0 +1,5 @@
+var c = document.getElementById("Main");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
