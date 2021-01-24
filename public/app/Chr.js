1f60fee12fd426f905652d1ba2945505663b65aa
mohamad nosratian <68176509+mohamadnosratian@users.noreply.github.com>
2021-01-25 01:09:37 +0330

    the base of rendering engine

--- public/app/Chr.js
+++ public/app/Chr.js
@@ -1,9 +1,23 @@
-export default (ctx, x, y) => {
-  ctx.fillStyle = "red";
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x + 100, y);
-  ctx.lineTo(x + 50, y + 100);
-  ctx.closePath();
-  ctx.fill();
-};
+class Gate {
+  constructor(ctx, x, y) {
+    this.x = x;
+    this.y = y;
+    this.ctx = /** @type {CanvasRenderingContext2D} */ (ctx);
+  }
+  drawUI() {
+    this.ctx.fillStyle = "#4e5251";
+    this.ctx.rect(this.x, this.y, 100, 50);
+    this.ctx.fill();
+  }
+  drawText() {
+    this.ctx.fillStyle = "#c2c2c2";
+    this.ctx.font = "30px Arial";
+    this.ctx.fillText("AND", this.x + 20, this.y + 35, 100);
+  }
+  updating(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export default Gate;
--- public/app/main.js
+++ public/app/main.js
@@ -1,7 +1,39 @@
-import f from "/app/Chr.js";
+import Gate from "/app/Chr.js";
 var c = /** @type {HTMLCanvasElement} */ (document.getElementById("Main"));
 var ctx = c.getContext("2d");
-c.height = 550;
-c.width = 1100;
 
-f(ctx, 500, 100);
+let HEIGHT = 550;
+let WIDTH = 1100;
+
+const f1 = new Gate(ctx, 50, 150);
+const f2 = new Gate(ctx, 50, 300);
+const f3 = new Gate(ctx, 200, 300);
+
+let Hierarchy = /** @type {Gate} */ ([f1, f2, f3]);
+
+function renderUI(e) {
+  Hierarchy.map((items) => {
+    items.drawUI();
+  });
+}
+function renderTexts(e) {
+  Hierarchy.map((items) => {
+    items.drawText();
+  });
+}
+window.addEventListener("click", (e) => {
+  console.log("click");
+  Hierarchy[0].updating(50, 40);
+  console.log(Hierarchy);
+});
+
+c.height = HEIGHT;
+c.width = WIDTH;
+
+function Render_Pipeline(e) {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  renderUI();
+  renderTexts();
+  requestAnimationFrame(Render_Pipeline);
+}
+Render_Pipeline();
